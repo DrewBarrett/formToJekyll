f625dceeec1aa88abccc93735e270b63d888359a
Drew <dfb1998@gmail.com>
2016-10-05 14:10:54 -0400

    some work

--- app.py
+++ app.py
@@ -2,11 +2,12 @@
 This script runs the application using a development server.
 It contains the definition of routes and views for the application.
 """
-
+import os
 from flask import Flask
 from flask import request
+from github import Github
 app = Flask(__name__)
-
+g = Github(os.environ.get("GIT_TOKEN"))
 # Make the WSGI interface available at the top level so wfastcgi can get it.
 wsgi_app = app.wsgi_app
 
@@ -17,7 +18,7 @@ def hello():
     if request.method == "GET":
         return "incorrect usage"
     else:
-        return "correct usage"
+        return "correct usage" + g.get_user()
 
 if __name__ == '__main__':
     import os
